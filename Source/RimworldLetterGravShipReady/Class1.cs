4c4cea599d30808ab39e8b71e6caeccd95f65ec0
thomas pegoraro <thomas.pegoraro@gmail.com>
2025-07-25 18:23:43 +1000

    Add project files.

---
+++ Source/RimworldLetterGravShipReady/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimworldLetterGravShipReady
+{
+    public class Class1
+    {
+    }
+}
